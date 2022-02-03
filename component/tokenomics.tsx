191b4afd20c936c7323a47259a212c4ae2a9bbf2
alfred <alfred.tupaz@myxlog.com>
2022-02-04 00:25:11 +0800

    fixes for UI

--- component/tokenomics.tsx
+++ component/tokenomics.tsx
@@ -12,12 +12,12 @@ const tokenomics = () => {
     {
         name: "Liquidity 20 years",
         class: "chart-item-0 rounded-full shadow-lg shadow-white stroke-indigo-600",
-        value: "5%"
+        value: "50%"
     },
     {
         name: "Staking pool",
         class: "chart-item-0 rounded-full shadow-lg shadow-white stroke-orange-700",
-        value: "5%"
+        value: "10%"
     },
     {
         name: "Marketing and development (Nft metaverse)",
@@ -27,7 +27,7 @@ const tokenomics = () => {
     {
         name: "Strategic partnership",
         class: "chart-item-0 rounded-full shadow-lg shadow-white stroke-pink-900",
-        value: "5%"
+        value: "10%"
     },
     {
         name: "Private sale",
@@ -42,7 +42,7 @@ const tokenomics = () => {
     {
         name: "Emergency fund",
         class: "chart-item-0 rounded-full shadow-lg shadow-white stroke-green-500",
-        value: "5%"
+        value: "10%"
     },
     ]
   return (
@@ -53,7 +53,7 @@ const tokenomics = () => {
             <div className="text-center w-full mb-4 ">
             <h3 className="text-4xl mb-8  text-center">Tokenomics</h3>
             </div>
-                <div className="grid lg:grid-cols-8 grid-cols-2 gap-4">
+                <div className="grid lg:grid-cols-8 grid-cols-2 gap-4 text-center">
                     {
                             tokenomics.map((element,key) => {
                             return (
@@ -61,10 +61,10 @@ const tokenomics = () => {
                                 <span className="mid">{element.value}</span>
                                 <svg transform="matrix(0 -1 -1 0 0 0)" className="bg-transparent rounded-full" width="120" height="120">
                                     <path d="M 112 60 A 52 52 0 0 0 17.931116292502736 29.435166880791392" className={element.class}  fill="transparent" stroke-width="16"></path>
-                                    <path d="M 17.931116292502736 29.435166880791392 A 52 52 0 0 0 43.93111629250272 109.45493884734799" className="chart-item-0 rounded-full stroke-blue-900"  fill="transparent" stroke-width="16"></path>
+                                    <path d="M 17.931116292502736 29.435166880791392 A 52 52 0 0 0 43.93111629250272 109.45493884734799" className={ element.value=='50%' ? element.class : "chart-item-0 rounded-full stroke-blue-900" }  fill="transparent" stroke-width="16"></path>
                                     <path d="M 43.93111629250272 109.45493884734799 A 52 52 0 0 0 112 60.000000000000014" className="chart-item-0 rounded-full stroke-blue-900"  fill="transparent" stroke-width="16"></path>
                                 </svg>
-                                <h1 className="mt-2">{ element.name }</h1>
+                                <h1 className="mt-2">{ element.name } <span className="lg:hidden block">({element.value})</span></h1>
                             </div>
                         )
                         })
